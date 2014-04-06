147f5ada19a4511a42c5a2674070c7275d872425
osager <osagerprairie@gmail.com>
2014-04-06 19:20:47 +0200

    xml-conduit: dw list of page and extract
    download a list a pages and extract information and write to file

--- xml-conduit/xml-conduit_examples.hs
+++ xml-conduit/xml-conduit_examples.hs
@@ -2,24 +2,31 @@
 module Main where
 
 import qualified Data.Text as T
-
-import Prelude hiding (readFile)
-import Text.HTML.DOM (readFile)
+import qualified Data.Text.IO as TIO
 
 import Text.XML.Cursor
-{-
 
-<h3>Accepted Orals</h3>
-<dl>
-    <dt>Reconstructing Storyline Graphs for Image Recommendation from Web Community Photos</dt>
-    <dd>Gunhee Kim* (Disney Research), Eric Xing (Carnegie Mellon University)</dd>
+import Network(withSocketsDo)
+import Network.HTTP.Conduit
+import Text.HTML.DOM
+import Data.List
 
--}
-filePath = "C:\\Users\\bjorn\\Documents\\GitHub\\haskell_lib_cookbook\\http-conduit\\CVPR 2014 papers on the web - Papers.htm"
+urlhead = "http://www.ibc.org/page.cfm/Action=ExhibList/ListID=1/PageNum="
+urltail = "/loadSearch=2096184_6720"
+
+allpages = [urlhead ++ (show x) ++ urltail |x<-[1..20]]
+
+cursorFor :: String -> IO Cursor
+cursorFor u = do
+  page <- simpleHttp u
+  return $ fromDocument $ parseLBS page
 
 main :: IO ()
-main = do
-    doc <- readFile filePath
-    let cursor = fromDocument doc
-    mapM_ print $ filter (not . T.isPrefixOf ("\n")) $
-        cursor $// element "dl" &// content
+main = withSocketsDo $ do
+  mapM cursorFor allpages >>= mapM_ eachCursor
+
+eachCursor cursor = do
+  mapM_ (TIO.appendFile "list.txt") (cursor $// findNodes)
+
+findNodes :: Cursor -> [T.Text]
+findNodes cursor = fmap (\x-> T.append x "\n") (cursor $// element "div" >=> attributeIs "class" "ez_companyname" &// content)
