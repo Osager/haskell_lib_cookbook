7cf3ed791a8960488b01c601d3c3b8d428abdf20
osager <osagerprairie@gmail.com>
2014-04-06 19:14:48 +0200

    http-conduit: delete trivial example

--- http-conduit/example.hs
+++
@@ -1,7 +0,0 @@
-import Network.HTTP.Conduit
-import qualified Data.ByteString.Lazy as L
-import Network (withSocketsDo)
-
-main :: IO ()
-main = withSocketsDo
-      $ simpleHttp "http://www.haskell.org/" >>= L.putStr
