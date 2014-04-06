ecb859703af80d732bbe052b50897b3969c1fe23
osager <osagerprairie@gmail.com>
2014-04-06 19:05:29 +0200

    http-conduit: down file from a url

---
+++ http-conduit/http-conduit_examples.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Network.HTTP.Conduit as C
+import Data.Conduit
+import qualified Data.Conduit.Binary as CB
+
+import Control.Monad.Trans.Resource
+import Control.Monad.IO.Class
+import qualified Data.Text as T
+import Network (withSocketsDo)
+
+import qualified Data.List.Split as S
+
+fileurl = "http://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg"
+
+downloadUrlFile :: T.Text -> IO ()
+downloadUrlFile link =  do
+    runResourceT $ do
+        manager <- liftIO $ C.newManager C.conduitManagerSettings
+        req <- liftIO $ C.parseUrl (T.unpack link)
+        res <- C.http req manager
+        C.responseBody res $$+- CB.sinkFile $ createFileName (T.unpack link)
+
+
+main :: IO()
+main = withSocketsDo $ 
+    downloadUrlFile fileurl
+
+--utilities
+createFileName :: String -> String
+createFileName raw = last $ S.splitOn "/" raw
