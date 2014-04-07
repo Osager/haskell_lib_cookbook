9f35ea76ea5dcfd5289e12849a1e774bd3ac5c63
osager <osagerprairie@gmail.com>
2014-04-07 13:44:58 +0200

    http-conduit: tidy up code a bit

--- http-conduit/http-conduit_examples.hs
+++ http-conduit/http-conduit_examples.hs
@@ -4,7 +4,7 @@ import qualified Network.HTTP.Conduit as C
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 
-import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Resource()
 import Control.Monad.IO.Class
 import qualified Data.Text as T
 import Network (withSocketsDo)
@@ -14,13 +14,12 @@ import qualified Data.List.Split as S
 fileurl = "http://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg"
 
 downloadUrlFile :: T.Text -> IO ()
-downloadUrlFile link =  do
-    runResourceT $ do
-        manager <- liftIO $ C.newManager C.conduitManagerSettings
-        req <- liftIO $ C.parseUrl (T.unpack link)
-        res <- C.http req manager
-        C.responseBody res $$+- CB.sinkFile $ createFileName (T.unpack link)
-
+downloadUrlFile link = runResourceT $
+    do manager <- liftIO $ C.newManager C.conduitManagerSettings
+       req <- liftIO $ C.parseUrl (T.unpack link)
+       res <- C.http req manager
+       C.responseBody res $$+- CB.sinkFile $
+        createFileName (T.unpack link)
 
 main :: IO()
 main = withSocketsDo $ 
