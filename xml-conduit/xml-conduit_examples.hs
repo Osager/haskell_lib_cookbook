bfab216516fbbb4cae1693b2aaf209d054e1a30c
osager <osagerprairie@gmail.com>
2014-04-08 19:43:08 +0200

    xml-conduit: more on stock prices

--- xml-conduit/xml-conduit_examples.hs
+++ xml-conduit/xml-conduit_examples.hs
@@ -23,30 +23,72 @@ urlInnate = "http://bourse.lesechos.fr/bourse/synthese-cotation-action-innate-ph
 urlPharming = "http://bourse.lesechos.fr/bourse/synthese-cotation-action-pharming-group-bourse-amsterdam,XAMS,PHARM,NL0010391025,ISIN.html"
 urlHeurtey = "http://bourse.lesechos.fr/bourse/synthese-cotation-action-heurtey-petrochem-bourse-paris,XPAR,ALHPC,FR0010343186,ISIN.html"
 
+urlBoursier = "http://www.boursier.com/"
+
 cursorFor :: String -> IO Cursor
 cursorFor u = do
 	page <- simpleHttp u
    	return $ fromDocument $ parseLBS page
 
-findNodes :: Cursor -> [Cursor]
-findNodes cursor = cursor $// element "div"
-						  >=> attributeIs "class" "b12-infosval"
-						  &// element "span"
+
+--
+findNodesEcho :: Cursor -> [Cursor]
+findNodesEcho cursor = cursor $// echoFilter
+
+echoFilter = element "div" >=> attributeIs "class" "b12-infosval"
+						   &// element "span"
+
+
+--
+findNodesBoursierTopVariation :: Cursor -> [Cursor]
+findNodesBoursierTopVariation cursor = cursor $// boursierFilterTopVariation
+
+boursierFilterTopVariation = element "div" >=> attributeIs "id" "palmares"
+						    			   &// element "td"
+							   			   >=> attributeIs "class" "tr"
+
+
+--
+findNodesBoursierTopName :: Cursor -> [Cursor]
+findNodesBoursierTopName cursor = cursor $// boursierFilterTopName
+
+boursierFilterTopName = element "div" >=> attributeIs "id" "palmares"
+								      &// element "tr"
+							   		  &// element "td"
+							   		  &// element "a"
+
+
+
 
 main :: IO ()
 main = withSocketsDo $ do
 	innateCursor <- cursorFor urlInnate
 	pharmingCursor <- cursorFor urlPharming
 	heurteyCursor <- cursorFor urlHeurtey
+	boursierCursor <- cursorFor urlBoursier
 
-	let innate_extractedInfo =  innateCursor $// findNodes &/ content
-	let pharming_extractedInfo =  pharmingCursor $// findNodes &/ content
-	let heurtey_extractedInfo =  heurteyCursor $// findNodes &/ content
+	let innate_extractedInfo =  innateCursor $// findNodesEcho &/ content
+	let pharming_extractedInfo =  pharmingCursor $// findNodesEcho &/ content
+	let heurtey_extractedInfo =  heurteyCursor $// findNodesEcho &/ content
+
+	let boursierBoursierTopName_extractedInfo =  boursierCursor $// findNodesBoursierTopName &/ content
+	let boursierBoursierTopVariation_extractedInfo =  boursierCursor $// findNodesBoursierTopVariation &/ content
+
+	putStrLn "Top 8:"
+	putStr "\n"
+
+	mapM_ print $ zipWith T.append (fmap (T.pack . (++ ":  ").show) [1..8]) (take 8 boursierBoursierTopName_extractedInfo)
+	mapM_ (print .T.strip) boursierBoursierTopVariation_extractedInfo
 
 	let innate_results = concat . take 1 $ mapM (T.splitOn "\r\n\t" . T.strip) $ take 10 innate_extractedInfo
 	let pharming_results = concat . take 1 $ mapM (T.splitOn "\r\n\t" . T.strip) $ take 10 pharming_extractedInfo
 	let heurtey_results = concat . take 1 $ mapM (T.splitOn "\r\n\t" . T.strip) $ take 10 heurtey_extractedInfo
 	
+
+	putStr "\n \n"
+	putStrLn "My Favorites:"
+	putStr "\n"
+
 	TIO.putStrLn $ T.append "     Name: "  (innate_results !! 0)
 	TIO.putStrLn $ T.append "Variation: "  (innate_results !! 3)
 	TIO.putStrLn $ T.append "    Price: "  (innate_results !! 4)
