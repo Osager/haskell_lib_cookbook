0c8bba1d6889160feb77d1f3524870f21d8c966c
osager <osagerprairie@gmail.com>
2014-04-10 20:12:07 +0200

    random: generate random coins
    how to declare a user type to be of random type

--- random/random_examples.hs
+++ random/random_examples.hs
@@ -6,7 +6,20 @@ instance (Random x, Random y, Random z) => Random (x, y,z) where
         (y, gen3) = randomR (y1, y2) gen2
         (z, gen4) = randomR (z1, z2) gen3
     in ((x, y,z), gen4)
+  random g = ((x,y,z),g3) where
+  	(x,g1) = random g
+  	(y,g2) = random g1
+  	(z,g3) = random g2
+
+data Coin = Head | Tail deriving (Show, Enum, Bounded)
+
+instance Random Coin where
+  randomR (a, b) g =
+    case randomR (fromEnum a, fromEnum b) g of
+      (x, g') -> (toEnum x, g')
+  random g = randomR (minBound, maxBound) g
 
 main = do
 	let pair = take 10 $ randomRs ((10,20,30),(60,40,60)) $ mkStdGen 10  :: [(Int,Int,Int)]
-	print pair
+	let rhead = take 100 $ randoms $ mkStdGen 1 :: [(Coin,Coin,Coin)]
+	print rhead
